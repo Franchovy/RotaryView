01faa1dbff3e033b678195eddcf9448c9173c90d
Maxime <maxrofra@gmail.com>
2023-02-15 17:37:04 +0100

    Initial Commit

---
+++ RotaryKnob/RotaryKnobApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotaryKnobApp.swift
+//  RotaryKnob
+//
+//  Created by Maxime Franchot on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RotaryKnobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
